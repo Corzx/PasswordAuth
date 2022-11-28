0b22830b99ea04c03f257b33441bf265b3df2876
xCorz <corey_239@hotmail.com>
2022-11-28 18:35:32 +1100

    final

--- PasswordAuth.py
+++ PasswordAuth.py
@@ -6,7 +6,13 @@ password = getpass.getpass("Please enter your password: ")
 
 for user in database.keys():
     if username == user:
-        while password != database.get(username):
+        while password != database.get(user):
             password = getpass.getpass("Please try again: ")
+            print("Access denied.")
+            break
+        if password in database.values():
+            print("Entry granted")
+            break
+    else:
+        print("Username does not exist.")
         break
-print("Entry granted")
